196a731bef6f38184f00fcd6d7def296cec2f985
prabhakarpandey76 <prabhakarpandey76@users.noreply.github.com>
2025-09-07 15:19:13 +0300

    Create appsconfig.py

---
+++ appsconfig.py
@@ -0,0 +1,27 @@
+"""
+DB SECTION
+"""
+
+
+#DB_SERVER = "marketsdb-instance-1.cuzwa7ub9atc.eu-west-2.rds.amazonaws.com"
+DB_SERVER = "localhost"
+DB_USER = "root"
+DB_PASS = ""
+DB_INSTANCE = "marketsdb"
+
+
+"""
+candlestick pattern detection configuration section
+"""
+
+PINBAR = 1
+REVERSEPINBAR = 2
+BUOB = 3 
+BEOB = 4
+DOJI = 5
+
+DAILY=0
+WEEKLY = 1
+MONTHLY = 2
+
+IMGDIR = "./images/"
